6d2798494f1ddf75c580e2a99b1d63b2f5aa0c41
eddie wang <wang8119@gmail.com>
2024-12-03 22:31:45 +0800

    feat: 當畫面過小時  <Logo>元件 會依據header 寬度等比縮小

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -4,9 +4,9 @@ import zeusLogo from '../../assets/zeus-logo.svg';
 const Logo: React.FC = () => {
   return (
     <a href="/">
-      <div className="flex items-center space-x-2">
+      <div className="flex scale-[0.75] items-center space-x-2 min-[350px]:scale-100">
         <img src={zeusLogo} alt="Zeus Guardian" className="h-[23px] w-[35px]" />
-        <div>
+        <div className="whitespace-nowrap">
           <h1 className="zeus-node-gradient">ZEUS NODE</h1>
           <p className="text-text-secondary text-17 font-extrabold">GUARDIAN</p>
         </div>
