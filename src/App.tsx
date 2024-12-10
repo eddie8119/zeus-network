19283d5a6b031a751621dd7022b7c01186f8b34b
eddie wang <wang8119@gmail.com>
2024-12-10 23:27:17 +0800

    feat: 增加假路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
@@ -7,6 +7,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/delegate" element={<Home />} />
+        <Route path="/delegations" element={<Home />} />
+        <Route path="/guardians" element={<Home />} />
+        <Route path="/doc" element={<Home />} />
       </Routes>
     </Router>
   );
--- src/components/header/EpochButton.tsx
+++ src/components/header/EpochButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ProgressBar from './../common/ProgressBar';
 import EpochPopup from './EpochPopup';
 
